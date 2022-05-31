69527c798fd7b64f0f35fec985addf52638058f8
KevinBaoB <kevinbaobui@gmail.com>
2022-05-31 07:32:37 -0500

    First

--- User.py
+++ User.py
@@ -1,1 +1,13 @@
 # your User class goes here
+class User:
+    def __init__(self, name, age, nationality):
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+
+    def __str__(self):
+        return f"My name is {self.name}, I'm {self.age} years old and I am {self.nationality}!"
+
+linda = User("Linda", 26, 'American')
+
+print(linda)
